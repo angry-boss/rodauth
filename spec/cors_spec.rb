b94bbc41dd0c018fcf86be4479d5a0f9ccaf48c0
Ilmira Shaikhraynova <ilmirashaikhraznova@gmail.com>
2020-05-22 13:58:50 +0200

    spec_helper

--- spec/cors_spec.rb
+++ spec/cors_spec.rb
@@ -1,4 +1,22 @@
-require_relative 'spec_helper'
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
+require 'simplecov'
+SimpleCov.minimum_coverage 100
+SimpleCov.start
+require 'minitest/autorun'
+require 'rack/test'
+
+require 'roda/plugins/cors'
+
+
+ENV['RACK_ENV'] = 'test'
+
+
+def create_app(&block)
+  c = Class.new(Roda)
+  c.class_eval(&block)
+  c
+end
 
 describe 'Cors' do
 
